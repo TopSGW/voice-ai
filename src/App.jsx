5752b2cbeb6f17f9cc11362e095e466c8c101363
TopSGW <web.smartdev22@gmail.com>
2025-02-25 21:20:53 -0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React from 'react'
 import AudioRecorder from './AudioRecorder'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </header>
       <h1>Speech to Text AI with Vite + React</h1>
       <div className="card">
         <AudioRecorder />
